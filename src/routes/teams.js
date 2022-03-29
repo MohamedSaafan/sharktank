0f8a7d66b52c4613492c658d601ede82fbf8e604
kreativekommit <mohamed@kreativekommit.com>
2022-03-29 14:13:28 +0200

    finished listing a single team

--- src/routes/teams.js
+++ src/routes/teams.js
@@ -1,8 +1,34 @@
+const pool = require("../config/db");
+
 const Router = require("express").Router;
 
 const router = new Router();
 
-router.get("/teams/:id/eventID", async (req, res, next) => {
-  res.send(req.params.id);
+router.get("/teams", (req, res, next) => {
+  res.send("teams");
+});
+router.get("/teams/:id/:eventID", async (req, res, next) => {
+  const teamID = req.params.id;
+  const eventID = req.params.eventID;
+  const teamQuery = await pool.query(
+    `
+  SELECT  teams.id as "id",
+    teams.name as "name",
+    sum(creatures.points) as "points"
+    FROM events 
+    JOIN teams ON team_a = teams.id OR team_b = teams.id 
+    JOIN creatures ON creatures.event_id = events.id AND creatures.team_id = teams.id
+    GROUP BY events.id , teams.id
+	having events.id = $1 and teams.id = $2;
+  `,
+    [eventID, teamID]
+  );
+  const eatenCreatures = await pool.query(`
+  SELECT id FROM creatures where id =1 and event_id = 1  and is_dead = true;
+  `);
+  const eatenCreaturesArray = eatenCreatures.rows.map((item) => item.id);
+  const team = teamQuery.rows[0];
+  team.eaten_creatures = eatenCreaturesArray;
+  res.send(team);
 });
 module.exports = router;
