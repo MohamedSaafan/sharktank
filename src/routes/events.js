06853334524c9dafef554bc7965077e87fe75534
kreativekommit <mohamed@kreativekommit.com>
2022-03-29 12:12:21 +0200

    added single fetch for the event

--- src/routes/events.js
+++ src/routes/events.js
@@ -2,7 +2,7 @@ const Router = require("express").Router;
 const pool = require("../config/db");
 const router = new Router();
 
-router.get("/api/v1/events/", async (req, res, next) => {
+const getEvents = async () => {
   const teamsQuery = await pool.query(
     `SELECT events.id as "eventId" ,
     events.is_scheduled as "isScheduled",
@@ -10,30 +10,27 @@ router.get("/api/v1/events/", async (req, res, next) => {
     teams.name as "teamName",
     teams.name, teams.id as "teamId",
     sum(creatures.points) as "points",
-        CASE  
-          WHEN events.is_scheduled = false THEN 'notScheduled'
-          WHEN events.is_scheduled is null THEN 'notScheduled' 
-          WHEN events.is_scheduled = true THEN 
-            CASE 
-              WHEN events.duration + events.schedule_date < now() + events.duration   
-                   OR 
-                   events.duration + events.schedule_date = now() + events.duration THEN 
-                     CASE WHEN events.duration + events.schedule_date < now() THEN 'finished' 
-                     ELSE 'live' END
-              WHEN events.duration + events.schedule_date > now() + events.duration AND events.is_scheduled = true THEN 'scheduled' END
-         END as "state"
+    CASE  
+      WHEN events.is_scheduled = false THEN 'notScheduled'
+      WHEN events.is_scheduled is null THEN 'notScheduled' 
+      WHEN events.is_scheduled = true THEN 
+        CASE 
+          WHEN events.duration + events.schedule_date < now() + events.duration   
+                OR 
+                events.duration + events.schedule_date = now() + events.duration THEN 
+                  CASE WHEN events.duration + events.schedule_date < now() THEN 'finished' 
+                  ELSE 'live' END
+          WHEN events.duration + events.schedule_date > now() + events.duration AND events.is_scheduled = true THEN 'scheduled' END
+      END as "state",
+    (events.schedule_date + events.duration) as "end_date"
     FROM events 
     JOIN teams ON team_a = teams.id OR team_b = teams.id 
     JOIN creatures ON creatures.event_id = events.id AND creatures.team_id = teams.id
     GROUP BY events.id , teams.id;  
      `
   );
-  console.log(
-    new Date(teamsQuery.rows[0].schedule_date).getTime(),
-    "from date"
-  );
+
   const events = teamsQuery.rows.map((row) => {
-    console.log(row, "from row");
     row.scheduledDate = new Date(row.scheduleDate).getTime();
     return row;
   });
@@ -44,30 +41,43 @@ router.get("/api/v1/events/", async (req, res, next) => {
     const object = {};
 
     events.forEach((item) => {
-      console.log(eventItem, item, "from item and event item");
       if (eventItem.eventId === item.id) {
         if (object.team_a) {
           object.team_b = {
             id: item.teamId,
             name: item.teamName,
-            points: item.points,
+            points: +item.points,
           };
         } else {
           object.team_a = {
             id: item.teamId,
             name: item.teamName,
-            points: item.points,
+            points: +item.points,
           };
           object.id = item.eventId;
-          object.date = item.scheduledDate;
+          object.schedule_date = item.scheduledDate;
+          object.end_date = new Date(item.end_date).getTime();
         }
       }
     });
-    console.log(new Date(object.date - new Date()), "from different time");
+
     return object;
   });
+  return mappedEvents;
+};
 
-  res.send(mappedEvents);
+router.get("/api/v1/events/", async (req, res, next) => {
+  const events = await getEvents();
+  res.send(events);
+});
+
+router.get("/api/v1/events/:id", async (req, res, next) => {
+  const events = await getEvents();
+  const filteredEvents = events.filter((event) => {
+    return event.id === +req.params.id;
+  });
+  console.log(filteredEvents, "from events");
+  res.send(filteredEvents[0]);
 });
 
 module.exports = router;
