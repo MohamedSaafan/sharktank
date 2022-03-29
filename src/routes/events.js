c8a7480cb769614787091926c7c5a7d84d7ddd11
kreativekommit <mohamed@kreativekommit.com>
2022-03-29 11:35:04 +0200

    finished getting the state of each event

--- src/routes/events.js
+++ src/routes/events.js
@@ -5,27 +5,27 @@ const router = new Router();
 router.get("/api/v1/events/", async (req, res, next) => {
   const teamsQuery = await pool.query(
     `SELECT events.id as "eventId" ,
-      events.is_scheduled as "isScheduled",
-      events.schedule_date as "scheduleDate" ,
-      teams.name as "teamName",
-      teams.name, teams.id as "teamId",
-      sum(creatures.points) as "points",
-          CASE  
-            WHEN events.is_scheduled = false THEN 'notScheduled'
-            WHEN events.is_scheduled is null THEN 'notScheduled' 
-            WHEN events.is_scheduled = true THEN 
-              CASE 
-                WHEN events.duration + events.schedule_date < now() + events.duration   
-                     OR 
-                     events.duration + events.schedule_date = now() + events.duration THEN 
-                       CASE events.duration + events.schedule_date < now() THEN 'finished' 
-                       ELSE 'live' END
-                WHEN events.duration + events.schedule_date > now() + events.duration AND events.is_scheduled = true THEN 'scheduled
-           END as "state"
-      FROM events 
-      JOIN teams ON team_a = teams.id OR team_b = teams.id 
-      JOIN creatures ON creatures.event_id = events.id AND creatures.team_id = teams.id
-      GROUP BY events.id , teams.id; 
+    events.is_scheduled as "isScheduled",
+    events.schedule_date as "scheduleDate" ,
+    teams.name as "teamName",
+    teams.name, teams.id as "teamId",
+    sum(creatures.points) as "points",
+        CASE  
+          WHEN events.is_scheduled = false THEN 'notScheduled'
+          WHEN events.is_scheduled is null THEN 'notScheduled' 
+          WHEN events.is_scheduled = true THEN 
+            CASE 
+              WHEN events.duration + events.schedule_date < now() + events.duration   
+                   OR 
+                   events.duration + events.schedule_date = now() + events.duration THEN 
+                     CASE WHEN events.duration + events.schedule_date < now() THEN 'finished' 
+                     ELSE 'live' END
+              WHEN events.duration + events.schedule_date > now() + events.duration AND events.is_scheduled = true THEN 'scheduled' END
+         END as "state"
+    FROM events 
+    JOIN teams ON team_a = teams.id OR team_b = teams.id 
+    JOIN creatures ON creatures.event_id = events.id AND creatures.team_id = teams.id
+    GROUP BY events.id , teams.id;  
      `
   );
   console.log(
