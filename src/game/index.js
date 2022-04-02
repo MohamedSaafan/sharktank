321be9a795c5137bc23783e461da4da235910f06
kreativekommit <mohamed@kreativekommit.com>
2022-04-02 11:47:54 +0200

    redo converting the game into class

--- src/game/index.js
+++ src/game/index.js
@@ -1,76 +1,74 @@
 const pool = require("../config/db");
-class Game {
-  getTwoTeams = async (event_id) => {
-    const getTeamsQuery = await pool.query(
-      `SELECT team_a,team_b FROM events WHERE id = $1`,
-      [event_id]
-    );
-    const teamAID = getTeamsQuery.rows[0].team_a;
-    const teamBID = getTeamsQuery.rows[0].team_b;
-    return { teamAID, teamBID };
-  };
 
-  getUnKilledCreatures = async (teamID, eventID) => {
-    const selectUnKilledQuery = await pool.query(
-      ` select id from creatures where team_id = $1 and  event_id = $2 and is_dead = false;`,
-      [teamID, eventID]
-    );
-    return selectUnKilledQuery.rows.map((row) => row.id);
-  };
+const getTwoTeams = async (event_id) => {
+  const getTeamsQuery = await pool.query(
+    `SELECT team_a,team_b FROM events WHERE id = $1`,
+    [event_id]
+  );
+  const teamAID = getTeamsQuery.rows[0].team_a;
+  const teamBID = getTeamsQuery.rows[0].team_b;
+  return { teamAID, teamBID };
+};
 
-  pickRandomCreature = (liveCreaturesIDs) => {
-    const selectedCreaturesIndeces = [];
-    // pick random 10 creatures and kill them
-    while (true) {
-      if (selectedCreaturesIndeces.length >= 10) break;
-      const randomIndex = Math.floor(Math.random() * liveCreaturesIDs);
-      const isExistInRandomIndex =
-        selectedCreaturesIndeces.indexOf(randomIndex) !== -1 ? true : false;
-      if (isExistInRandomIndex) continue;
-      selectedCreaturesIndeces.push(randomIndex);
-    }
-    return selectedCreaturesIndeces;
-  };
+const getUnKilledCreatures = async (teamID, eventID) => {
+  const selectUnKilledQuery = await pool.query(
+    ` select id from creatures where team_id = $1 and  event_id = $2 and is_dead = false;`,
+    [teamID, eventID]
+  );
+  return selectUnKilledQuery.rows.map((row) => row.id);
+};
 
-  killCreatures = async (eventID, teamID) => {
-    const liveCreaturesIDs = getUnKilledCreatures(teamID, eventID);
-    if (liveCreaturesIDs.length < 10) {
-      // kill all of them
-      const killCreaturesQuery = await pool.query(
-        `UPDATE CREATURES SET is_dead = true where id IN (${liveCreaturesIDs.toString()})`
-      );
-      return liveCreaturesIDs;
-    }
+const pickRandomCreature = (liveCreaturesIDs) => {
+  const selectedCreaturesIndeces = [];
+  // pick random 10 creatures and kill them
+  while (true) {
+    if (selectedCreaturesIndeces.length >= 10) break;
+    const randomIndex = Math.floor(Math.random() * liveCreaturesIDs);
+    const isExistInRandomIndex =
+      selectedCreaturesIndeces.indexOf(randomIndex) !== -1 ? true : false;
+    if (isExistInRandomIndex) continue;
+    selectedCreaturesIndeces.push(randomIndex);
+  }
+  return selectedCreaturesIndeces;
+};
 
-    const selectedCreaturesIndeces = pickRandomCreature(liveCreaturesIDs);
-    // now we need to retreive the ids in their own array
-    const selectedCreaturesIDs = selectedCreaturesIndeces.map(
-      (item) => liveCreaturesIDs[item]
-    );
-    // kill the creatures
-    const desiredCreaturesIDsString = selectedCreaturesIDs.toString();
+const killCreatures = async (eventID, teamID) => {
+  const liveCreaturesIDs = getUnKilledCreatures(teamID, eventID);
+  if (liveCreaturesIDs.length < 10) {
+    // kill all of them
     const killCreaturesQuery = await pool.query(
-      `UPDATE CREATURES SET is_dead = true WHERE id IN ${desiredCreaturesIDsString}`
+      `UPDATE CREATURES SET is_dead = true where id IN (${liveCreaturesIDs.toString()})`
     );
-    return desiredCreaturesIDsString;
-  };
-  rewardCreatures = async (eventID) => {
-    await pool.query(
-      `UPDATE creatures set points = points + 1 WHERE is_killed = false AND event_id = $1`,
-      eventID
-    );
-  };
+    return liveCreaturesIDs;
+  }
 
-  startGame = (eventID) => {
-    const { teamAID, teamBID } = getTwoTeams(eventID);
-    /// run eat creatures function every 1 hour
-    const killCreatureIntervalKey = setInterval(async () => {
-      await killCreatures(eventID, teamAID);
-      await killCreatures(eventID, teamBID);
-      await rewardCreatures(eventID);
-    }, 60000);
-    //3 600 000 milli seconds equals one hour
+  const selectedCreaturesIndeces = pickRandomCreature(liveCreaturesIDs);
+  // now we need to retreive the ids in their own array
+  const selectedCreaturesIDs = selectedCreaturesIndeces.map(
+    (item) => liveCreaturesIDs[item]
+  );
+  // kill the creatures
+  const desiredCreaturesIDsString = selectedCreaturesIDs.toString();
+  const killCreaturesQuery = await pool.query(
+    `UPDATE CREATURES SET is_dead = true WHERE id IN ${desiredCreaturesIDsString}`
+  );
+  return desiredCreaturesIDsString;
+};
+const rewardCreatures = async (eventID) => {
+  await pool.query(
+    `UPDATE creatures set points = points + 1 WHERE is_killed = false AND event_id = $1`,
+    eventID
+  );
+};
 
-    // run reward creatures every hour after the eat creatures passed
-  };
-}
+const startGame = (eventID) => {
+  const { teamAID, teamBID } = getTwoTeams(eventID);
+  /// run eat creatures function every 1 hour
+  const killCreatureIntervalKey = setInterval(async () => {
+    await killCreatures(eventID, teamAID);
+    await killCreatures(eventID, teamBID);
+    await rewardCreatures(eventID);
+  }, 60000);
+  //3 600 000 milli seconds equals one hour
+  // run reward creatures every hour after the eat creatures passed
+};
