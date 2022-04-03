a7b114c62312f10957f519294a51e95fe1aea3af
kreativekommit <mohamed@kreativekommit.com>
2022-04-03 13:14:46 +0200

    finished add Creatures query

--- src/game/addCreatures.js
+++ src/game/addCreatures.js
@@ -1,11 +1,34 @@
 const pool = require("../config/db.js");
 
-const addCreatures = async () => {
-  const addQuery = await pool.query(`
+const addCreatures = async (
+  eventId,
+  address,
+  teamID,
+  isPicked,
+  timePicked,
+  isDead
+) => {
+  const addQuery = await pool.query(
+    `
    INSERT INTO CREATURES (
-
+    event_id,
+    address,
+    points,
+    team_id,
+    is_picked,
+    time_picked,
+    is_dead 
    ) VALUES (
-
-   )
-   `);
+       $1,
+       $2,
+       0,
+       $3,
+       $4,
+       $5,
+       $6
+    
+   );
+   `,
+    [eventId, address, teamID, isPicked, timePicked, isDead]
+  );
 };
