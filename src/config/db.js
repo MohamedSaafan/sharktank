053f0d61d1ab906ccede398a54e22530405d92e3
kreativekommit <mohamed@kreativekommit.com>
2022-04-07 17:58:03 +0200

    changed the port to be automatic by heroku

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,3 @@
-console.log("\n\n", process.env.DATABASE_URL, "\n\nfrom database url \n\n");
-process.env.DATABASE_URL
-  ? console.log("\n\nrequiring db for production\n\n")
-  : console.log("\n\n requiring for development \n\n");
 module.exports = process.env.DATABASE_URL
   ? require("./db_production")
   : require("./db_local");
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use("/api/v1", router);
-
-app.listen(8080, () => {
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
   console.log("app runing sucesffully in port 8080");
 });
