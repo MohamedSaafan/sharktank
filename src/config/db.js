1b327b13fe26b90fbe4ad42ac4eff10344e5f4e7
kreativekommit <mohamed@kreativekommit.com>
2022-04-07 17:52:31 +0200

    logged what to require

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,7 @@
 console.log("\n\n", process.env.DATABASE_URL, "\n\nfrom database url \n\n");
-
+process.env.DATABASE_URL
+  ? console.log("\n\nrequiring db for production\n\n")
+  : console.log("\n\n requiring for development \n\n");
 module.exports = process.env.DATABASE_URL
   ? require("./db_production")
   : require("./db_local");
