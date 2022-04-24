ad8c38b0e8a39af16ba2eb3c9b1154baa21b5141
kreativekommit <mohamed@kreativekommit.com>
2022-04-24 16:14:28 +0200

    fixed the problem of unpassing the parameter to the pull query

--- src/routes/events.js
+++ src/routes/events.js
@@ -99,10 +99,15 @@ router.post("/events/:id/pull/:creature_id", async (req, res, next) => {
   const eventID = req.params.id;
   const creatureID = req.params.creature_id;
   const updateCreatureQuery = await pool.query(
-    `UPDATE creatures SET is_picked = true where id = $1 and event_id = $2`
+    `UPDATE creatures SET is_picked = true where id = $1 and event_id = $2`,
+    [creatureID, eventID]
   );
   sendMessageForClients(
-    JSON.stringify({ event_id: eventID, creature_id: creatureID })
+    JSON.stringify({
+      event_id: eventID,
+      creature_id: creatureID,
+      type: "fish picked",
+    })
   );
   res.status(201).send({ message: "Fish Picked successfully" });
 });
