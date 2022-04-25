308d1fa97532badd99ab9a763a3b6efbbe02bd58
kreativekommit <mohamed@kreativekommit.com>
2022-04-25 15:12:18 +0200

    adjusted the get online events to return the team id

--- src/routes/players.js
+++ src/routes/players.js
@@ -6,19 +6,19 @@ router.get("/:address/online-events", async (req, res, next) => {
   const playerAddress = req.params.address;
   // we need to get all the events the player has a creature in
   const eventsQuery = await pool.query(
-    `SELECT DISTINCT event_id
-     FROM creatures JOIN events ON creatures.event_id = events.id
-     where address = $1 
-     AND 
-     events.schedule_date <= now() 
-     AND events.finished = false
-     AND events.is_scheduled = true;
+    `SELECT DISTINCT event_id,creatures.team_id
+    FROM creatures JOIN events ON creatures.event_id = events.id
+    where address = $1
+    AND 
+    events.schedule_date <= now() 
+    AND events.finished = false
+    OR events.finished = null
+    AND events.is_scheduled = true;
      `,
     [playerAddress]
   );
-  if (eventsQuery.rowCount === 0) return res.send([]);
-  const eventsIDsArray = eventsQuery.rows.map((row) => row.event_id);
-  res.send(eventsIDsArray);
+
+  res.send(eventsQuery.rows);
 });
 
 module.exports = router;
