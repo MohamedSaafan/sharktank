1ddd90d33ebda19ca5c12e7fb127e983b6acfc97
kreativekommit <mohamed@kreativekommit.com>
2022-05-08 20:08:59 +0200

    returned the joined property along with the online-events

--- src/routes/players.js
+++ src/routes/players.js
@@ -4,6 +4,11 @@ const router = new Router();
 
 router.get("/:address/online-events", async (req, res, next) => {
   const playerAddress = req.params.address;
+  if (!playerAddress)
+    return res
+      .status(400)
+      .send({ message: "Error You Should Provide a user address" });
+
   // we need to get all the events the player has a creature in
   const eventsQuery = await pool.query(
     `SELECT DISTINCT event_id,creatures.team_id
@@ -17,11 +22,33 @@ router.get("/:address/online-events", async (req, res, next) => {
      `,
     [playerAddress]
   );
+
   const onlineEvents = eventsQuery.rows;
+  const joinedEventsQuery = await pool.query(
+    `SELECT DISTINCT event_id FROM creatures WHERE address = $1 and joined = true `,
+    [playerAddress]
+  );
+  const joinedEvents = joinedEventsQuery.rows;
+  // check the edge case where there are no joined queries
+  if (!joinedEventsQuery.rowCount) {
+    onlineEvents.map((item) => {
+      item.joined = false;
+      return item;
+    });
+    res.status(200).send(onlineEvents);
+    return;
+  }
+  // mark the joined events as joined
 
-  // after getting the creatures we need to see the IDs of the events he joined in mark them as joined
+  onlineEvents.map((event) => {
+    joinedEvents.forEach((joinedEvent) => {
+      if (event.event_id === joinedEvent.event_id) return (event.joined = true);
+      event.joined = false;
+    });
+    return event;
+  });
 
-  res.send(eventsQuery.rows);
+  res.status(200).send(onlineEvents);
 });
 
 module.exports = router;
