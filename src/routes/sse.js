d0ab8372108ddd5388bd7d1039e11fa85a168bc4
kreativekommit <mohamed@kreativekommit.com>
2022-05-25 15:18:34 +0200

    returned a response once the client subscribed

--- src/routes/sse.js
+++ src/routes/sse.js
@@ -3,6 +3,8 @@ const sseReqArray = [];
 
 router.get("/monitor", (req, res, next) => {
   res.setHeader("Content-Type", "text/event-stream");
+  const message = "you have subscribed sucessfully";
+  res.write(`data: ${message}\n\n`);
   sseReqArray.push(res);
   // const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   // sseReqArray.filter((req) => {
